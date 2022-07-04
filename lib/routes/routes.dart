b69653474e3c5c82bc8c8494d438fe03e69d3e8c
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2022-07-04 09:53:21 +0500

    add

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,3 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:map_dawnlod_camera_pagination/screens/home/my_home_page.dart';
+
 class Routess {
-  
+  static final Routess _instanse = Routess.init();
+  static Routess get instanse => Routess._instanse;
+  Routess.init();
+  Route? onGenerate(RouteSettings settings) {
+    var args = settings.arguments;
+    switch (settings.arguments) {
+      case "/":
+        return MaterialPageRoute(builder: (context) => MyHomePage());
+    }
+  }
 }
